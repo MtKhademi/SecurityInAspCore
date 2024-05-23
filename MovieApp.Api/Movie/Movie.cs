140b58e1e6f20f07783810b24f3344ec6951f21e
Mohande3Khademi <Mohande3Khademi@hotmail.com>
2024-05-23 10:47:35 +0330

    Add project files.

---
+++ MovieApp.Api/Movie/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApp.Api.Movie
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedDateTime { get; set; }
+    }
+}
---
+++ MovieApp.Api/Movie/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Api.Movie
+{
+    public class MovieController : ControllerBase
+    { 
+
+        [HttpGet]
+        public async Task<IEnumerable<Movie>> GetMoviews()
+        {
+            return null;
+        }
+
+    }
+}
