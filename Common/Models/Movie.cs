7af5afa416abc5d5fd16535084e1ef927499ae87
Mohande3Khademi <Mohande3Khademi@hotmail.com>
2024-05-23 10:50:44 +0330

    feat: add first project for authenitcation built-in ASP core

---
+++ Common/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedDateTime { get; set; }
+    }
+}
